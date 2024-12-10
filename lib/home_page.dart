83c11197930da465600b7c10ce8c453818723da8
MerrrH1 <mervinhowardd38@gmail.com>
2024-12-11 06:29:00 +0700

    Updated UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_api_demo/models/product_models.dart';
 import 'package:latihan_api_demo/repository/product_repository.dart';
 import 'package:latihan_api_demo/response/product_response.dart';
 
@@ -20,20 +21,43 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget build(BuildContext context) {
+    productRepository.getProducts();
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan API Demo"),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        body: FutureBuilder(
-            future: futureProducts,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(itemCount: snapshot.data!.listProduct.length,
-                itemBuilder: (context, index) {
-                  
-                })
-              }
-        }));
+      appBar: AppBar(
+        title: Text("Latihan API Demo"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: FutureBuilder(
+          future: futureProducts,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.listProduct.length,
+                  itemBuilder: (context, index) {
+                    ProductModel productModel =
+                        snapshot.data!.listProduct[index];
+                    debugPrint(productModel.title);
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: Text(productModel.id.toString()),
+                      ),
+                      title: Text(productModel.title),
+                      subtitle: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(productModel.stock.toString()),
+                          Text("\$ " + productModel.price.toString())
+                        ],
+                      ),
+                    );
+                  });
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            return CircularProgressIndicator();
+          }),
+    );
   }
 }
